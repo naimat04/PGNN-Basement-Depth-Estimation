70f0c8e63e823dad4129d0bd312b0e06106bc35a
Kuldeep Sarkar <kuldeepsarkariit39@gmail.com>
2025-09-05 22:10:23 +0530

    Update Error.py

--- physics_informed_gravity_inversion/Error.py
+++ physics_informed_gravity_inversion/Error.py
@@ -5,5 +5,7 @@ model.compile(optimizer='adam', loss=PhysicsLoss(alpha=0.7))
 
 model.fit(X_train, y_train, validation_data=(X_valid, y_valid), epochs=100, batch_size=32)
 
-
+X_train = X_train[..., np.newaxis]  # shape (N,101,101,1)
+depth_train = depth_train[..., np.newaxis]
+gravity_train = gravity_train[..., np.newaxis]
 
