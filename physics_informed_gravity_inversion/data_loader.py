8eb384f54af3a3c25dc45e15cbbda0c210d708a3
Naimatullah <108405318+naimat04@users.noreply.github.com>
2025-09-06 14:34:48 +0530

    Update data_loader.py

--- physics_informed_gravity_inversion/data_loader.py
+++ physics_informed_gravity_inversion/data_loader.py
@@ -3,19 +3,27 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 def load_data():
-    depth=scipy.io.loadmat('gravity_depth_field_21.mat')['depth']
-    gravity=scipy.io.loadmat('gravity_depth_field_21.mat')['gravity']
-    depthN=depth/10000
-    
-    # data split
-    X_train_main, X_test, y_train_main, y_True = train_test_split(gravity, depthN, test_size=0.3, random_state=155)
-    X_train, X_valid, y_train, y_valid = train_test_split(X_train_main, y_train_main, test_size=0.3, random_state=155)
-    
-    X_test=np.resize(X_test,(X_test.shape[0],101,101))
-    y_True=np.resize(y_True,(y_True.shape[0],101,101))
-    X_valid=np.resize(X_valid,(X_valid.shape[0],101,101))
-    y_valid=np.resize(y_valid,(y_valid.shape[0],101,101))
-    y_train=np.resize(y_train,(y_train.shape[0],101,101))
-    X_train=np.resize(X_train,(X_train.shape[0],101,101))
-    
-    return X_train, X_valid, X_test, y_train, y_valid, y_True
+    depth=scipy.io.loadmat('exp_data.mat')['depthExp']
+    gravity=scipy.io.loadmat('exp_data.mat')['gravityExp']
+    depthN = depth / 10000
+
+    # Split
+    X_train, X_test, y_train, y_True = train_test_split(gravity, depthN, test_size=0.3, random_state=155)
+    X_valid, X_test, y_valid, y_True = train_test_split(X_test, y_True, test_size=0.3, random_state=155)
+
+    # Reshape
+    def reshape(data):
+        return np.expand_dims(np.resize(data, (data.shape[0], 101, 101)), axis=-1)
+
+    gravity_train = reshape(X_train)
+    gravity_valid = reshape(X_valid)
+    gravity_test = reshape(X_test)
+
+    depth_train = reshape(y_train)
+    depth_valid = reshape(y_valid)
+    depth_true = reshape(y_True)
+
+    y_train = np.concatenate([depth_train, gravity_train], axis=-1)
+    y_valid = np.concatenate([depth_valid, gravity_valid], axis=-1)
+    y_true  = np.concatenate([depth_true,  gravity_test], axis=-1)
+    return gravity_train, gravity_valid, gravity_test, y_train, y_valid, y_true
